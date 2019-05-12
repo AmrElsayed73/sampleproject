757a1f69132b2648f9ef480409d5ebcce324a86f
root <root@Jenkins-Slave.xjukp1vqy3oupmno1orqdbwitg.ax.internal.cloudapp.net>
2019-05-12 09:14:30 +0000

    first commit

---
+++ myapp/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example Title</title>
+    </head>
+    <body>
+        <p>
+            Hello. Today is <?= date('l \t\h\e jS') ?>.<br>
+	    This is all test hello world 
+        </p>
+    </body>
+</html>
