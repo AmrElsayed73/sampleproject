d8e80f5ba70ba262afcf8b8308e32aa5ee495245
root <root@Jenkins-Slave.xjukp1vqy3oupmno1orqdbwitg.ax.internal.cloudapp.net>
2019-05-12 09:24:53 +0000

    new

--- myapp/index.php
+++ myapp/index.php
@@ -6,7 +6,8 @@
     <body>
         <p>
             Hello. Today is <?= date('l \t\h\e jS') ?>.<br>
-	    This is all test hello world 
+	    This is all test hello world <br>
+	    Build for Amr
         </p>
     </body>
 </html>
